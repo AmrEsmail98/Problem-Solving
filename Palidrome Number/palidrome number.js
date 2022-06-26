46faaf9110bbbc3fb9195cc667c666c94025f58b
amroesmail98 <amroesmail98@gmail.com>
2022-06-26 17:17:00 +0200

    Problem-Solving

---
+++ Palidrome Number/palidrome number.js
@@ -0,0 +1,7 @@
+function ispalidrome(x){
+    if(x<0) return false;
+    let reverse =0;
+    for(let i=x; i>=1;i=Math.floor(i/10))
+    reverse=reverse*10 +i%10;
+    return  reverse === x;
+}
---
+++ two sum/twosum.js
@@ -0,0 +1,16 @@
+
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+var twosum = function (nums, target) {
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; i < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+};
+
